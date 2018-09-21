e4806a3d7e0d0663f4250debe80a532c934dd071
raymonddee <raymonddee@hotmail.com>
2018-09-21 16:40:40 -0400

    [ADD] Main "GameEditor" scene and meshes

---
+++ Engines Assignment 1/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position.x += Input.GetAxis("Horizontal") * 10 * Time.deltaTime;
+	}
+}
