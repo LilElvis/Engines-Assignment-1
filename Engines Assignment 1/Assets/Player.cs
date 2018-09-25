dfb99c6cef218842659d0ce0fddcfd2576a5628e
raymond dee <43120753+LilElvis@users.noreply.github.com>
2018-09-25 11:03:04 -0400

    [ADD] ACtually added all that stuff this time, my bad.

---
+++ Engines Assignment 1/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public bool hasWon;
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+            hasWon = true;
+    }
+}
