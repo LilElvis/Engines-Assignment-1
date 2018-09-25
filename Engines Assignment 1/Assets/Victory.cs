f9d7c701b8020f2c2893b399d608957ca3b8e876
raymond dee <raymond.dee@uoit.net>
2018-09-25 11:03:04 -0400

    [ADD] ACtually added all that stuff this time, my bad.

---
+++ Engines Assignment 1/Assets/Victory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour {
+
+    private GameObject m_Player;
+    void Start ()
+    {
+        m_Player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(m_Player.GetComponent<Player>().hasWon)
+        {
+            Debug.Log("YOU WIN");
+        }
+	}
+}
