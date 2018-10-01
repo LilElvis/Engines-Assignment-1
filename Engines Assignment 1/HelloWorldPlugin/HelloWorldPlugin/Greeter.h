caf896be92559d4338b8ea100c7f2d4a1b9b2384
raymond dee <raymond.dee@uoit.net>
2018-10-01 19:27:53 -0400

    [ADD] Plugin Project

---
+++ Engines Assignment 1/HelloWorldPlugin/HelloWorldPlugin/Greeter.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "LibSettings.h"
+#include <string>
+
+class LIB_API Greeter
+{
+public:
+	char* greet();
+
+	void setGreeting(std::string);
+
+	int add(int, int);
+
+private:
+	std::string* greeting;
+};
